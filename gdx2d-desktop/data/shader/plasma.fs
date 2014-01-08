b8e50968e1b57523bc087eca83e5537df6db864d
Pierre-André Mudry <pmudry@gmail.com>
2014-01-08 21:19:41 +0100

    New branch for Opengl shader integration

---
+++ gdx2d-desktop/data/shader/plasma.fs
@@ -0,0 +1,24 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.1415926535897932384626433832795
+ 
+uniform float time;
+//uniform vec2 u_k;
+const vec2 u_k = vec2(1.0, 1.0);
+varying vec2 surfacePosition;
+ 
+void main() {
+    float v = 0.0;
+    vec2 c = surfacePosition * u_k - u_k/2.0;
+    v += sin((c.x+time));
+    v += sin((c.y+time)/2.0);
+    v += sin((c.x+c.y+time)/2.0);
+    c += u_k/2.0 * vec2(sin(time/3.0), cos(time/2.0));
+    v += sin(sqrt(c.x*c.x+c.y*c.y+1.0)+time);
+    v = v/2.0;
+    vec3 col = vec3(1, sin(PI*v), cos(PI*v));
+    
+    gl_FragColor = vec4(col*.5 + .5, 1);
+}
---
+++ gdx2d-desktop/data/shader/watermix.fs
@@ -0,0 +1,29 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+uniform float time;
+uniform vec2 resolution;
+
+const float Pi = 3.141592657;
+const int zoom = 90;
+const float speed = .5;
+float fScale = 0.85;
+
+void main(void)
+{
+	
+	vec2 uv = gl_FragCoord.xy / resolution.xy;
+	vec2 p=(4.0*gl_FragCoord.xy-resolution.xy)/max(resolution.x,resolution.y);
+	
+	float ct = time * speed * 25.0;
+	
+	for(int i=1;i<zoom;i++) {
+		vec2 newp=p;
+		newp.x+=0.25/float(i)*cos(float(i)*p.y+time*cos(ct)*0.3/40.0+0.03*float(i))*fScale+10.0;		
+		newp.y+=0.5/float(i)*cos(float(i)*p.x+time*ct*0.3/50.0+0.03*float(i+10))*fScale+15.0;
+		p=newp;
+	}
+	
+	vec3 col=vec3(0.5*sin(3.0*p.x)+0.5, 0.5*sin(3.0*p.y)+0.5, sin(p.x+p.y));
+	gl_FragColor=vec4(col, 1.0);
+}
