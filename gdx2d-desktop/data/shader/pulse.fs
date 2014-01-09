00e2774ed4d9e72431beb36ea3e4a18e0fe7b94a
Pierre-André Mudry <pierre-andre.mudry@hevs.ch>
2014-01-09 15:35:34 +0100

    Cast error on Linux, corrected

--- gdx2d-desktop/data/shader/pulse.fs
+++ gdx2d-desktop/data/shader/pulse.fs
@@ -3,7 +3,7 @@ uniform vec2 resolution;
 
 void main( void ) {
 	vec2 p = ( gl_FragCoord.xy / resolution.xy ) - 0.5;
-	float s = sin(time*2);
+	float s = sin(time*2.0f);
 	float dy = 1. / ( 55. * abs(sin(p.y)*s) );
 	gl_FragColor = vec4( dy * 0.1 * dy, 0.5 * dy, dy, 1.0 );
 }
