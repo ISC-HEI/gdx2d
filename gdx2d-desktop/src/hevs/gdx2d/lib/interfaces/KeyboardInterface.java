6ce52ea1ca0391ae9c7e7a60f359148a53edde3e
Pierre-André Mudry <pmudry@gmail.com>
2014-01-21 23:16:54 +0100

    Added proper heading

--- gdx2d-desktop/src/hevs/gdx2d/lib/interfaces/KeyboardInterface.java
+++ gdx2d-desktop/src/hevs/gdx2d/lib/interfaces/KeyboardInterface.java
@@ -1,14 +1,17 @@
 package hevs.gdx2d.lib.interfaces;
 
+/**
+ * An interface to interact with the application using a keyboard 
+ * @author Pierre-André Mudry (mui)
+ */
 public interface KeyboardInterface {
-
-	/*
-	 * Invoked when a key is typed. Use Input.Keys.xy to read the keycode
+	/**
+	 * Invoked when a key is typed. Use Input.Keys.xy to read the key code
 	 */
 	public abstract void onKeyDown(int keycode);
 
-	/*
-	 * Invoked when a key is released. Use Input.Keys.xy to read the keycode
+	/**
+	 * Invoked when a key is released. Use Input.Keys.xy to read the key code
 	 */
 	public abstract void onKeyUp(int keycode);
 
--- gdx2d-desktop/src/hevs/gdx2d/lib/interfaces/TouchInterface.java
+++ gdx2d-desktop/src/hevs/gdx2d/lib/interfaces/TouchInterface.java
@@ -1,17 +1,22 @@
 package hevs.gdx2d.lib.interfaces;
 
+/**
+ * An interface that applications implement 
+ * to interact with the user using mouse or touch
+ *  @author Pierre-André Mudry (mui)
+ */
 public interface TouchInterface {
-	/*
+	/**
 	 * Invoked when the pointer (mouse or touch) is "clicked"
 	 */
 	public abstract void onClick(int x, int y, int button);
 	
-	/*
+	/**
 	 * Invoked when the pointer (mouse or touch) is moved and down
 	 */
 	public abstract void onDrag(int x, int y);
 
-	/*
+	/**
 	 * Invoked when the pointer (mouse or touch) is released
 	 */
 	public abstract void onRelease(int x, int y, int button);
