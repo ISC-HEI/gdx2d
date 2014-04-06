5b5e1d32dcab639fd53c39cbe9fbcdbbf6201f4c
Pierre-André Mudry <pmudry@gmail.com>
2014-04-06 13:34:06 +0200

    Documentation effort, ctd

--- gdx2d-desktop/src/hevs/gdx2d/components/colors/ColorUtils.java
+++ gdx2d-desktop/src/hevs/gdx2d/components/colors/ColorUtils.java
@@ -3,7 +3,9 @@ package hevs.gdx2d.components.colors;
 import com.badlogic.gdx.graphics.Color;
 
 /**
- * Color conversion utilities. This was originally taken on 
+ * Color conversion utilities methods.
+ * 
+ * <br>The source code was adapted from a question on 
  * <a href=http://stackoverflow.com/questions/7896280/converting-from-hsv-hsb-in-java-to-rgb-without-using-java-awt-color-disallowe"> stack overflow</a>
  * 
  * @author Pierre-André Mudry (mui)
@@ -11,22 +13,23 @@ import com.badlogic.gdx.graphics.Color;
  */
 public class ColorUtils {	
 	/**
-	 * Conversion from the HSV color space to {@link Color}
-	 * @param hue
-	 * @param saturation
-	 * @param value
-	 * @return
+	 * Conversion from the HSV color space to {@link Color}.
+	 * @param hue Hue of the original color (tint)
+	 * @param saturation The amount of color saturation 
+	 * @param value Value of the current color
+	 * @return The converted {@link Color}
 	 */
 	public static Color hsvToColor(float hue, float saturation, float value) {
 		return intToColor(hsvToRgb(hue, saturation, value));
 	}
 	
 	/**
-	 * Converts hsv to rgb int
-	 * @param hue
-	 * @param saturation
-	 * @param value
-	 * @return
+	 * Converts hue-saturation-value color space to red-green-blue integer
+	 * value representation.
+	 * @param hue Hue of the original color (tint)
+	 * @param saturation The amount of color saturation 
+	 * @param value Value of the current color
+	 * @return The integer representation of the {@link Color}
 	 */
 	public static int hsvToRgb(float hue, float saturation, float value) {
 
@@ -48,11 +51,11 @@ public class ColorUtils {
 	}
 
 	/**
-	 * Creates an int representation of rgb color
-	 * @param r
-	 * @param g
-	 * @param b
-	 * @return
+	 * Converts an RGB color to its integer counterpart
+	 * @param r the red component 
+	 * @param g the green component
+	 * @param b the blue component
+	 * @return the integer value representing this color
 	 */
 	public static int rgbToInt(float r, float g, float b){
 		return Color.rgb888(r, g, b);
@@ -60,8 +63,8 @@ public class ColorUtils {
 	
 	/**
 	 * Creates a {@link Color} from an integer value
-	 * @param col
-	 * @return 
+	 * @param col the integer color value to convert
+	 * @return the {@link Color} representation of the given integer value 
 	 */
 	public static Color intToColor(int col){
 		float r = ((col & 0x00ff0000) >>> 16) / 255f;
