0dda70ef48dbf3d29c3eb10c3fc7c8b8871697f8
Pierre-André Mudry <pmudry@gmail.com>
2014-05-12 09:53:07 +0200

    Removed unnecessary comment

--- gdx2d-desktop/src/hevs/gdx2d/demos/physics/DemoSimplePhysics.java
+++ gdx2d-desktop/src/hevs/gdx2d/demos/physics/DemoSimplePhysics.java
@@ -10,7 +10,6 @@ import hevs.gdx2d.lib.physics.DebugRenderer;
 import hevs.gdx2d.lib.physics.PhysicsWorld;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
