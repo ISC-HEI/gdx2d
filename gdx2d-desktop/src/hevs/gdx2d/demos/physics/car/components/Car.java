6b66a401f8dbc863cbd5b65dc24d4d7b7265a520
Pierre-André Mudry <pmudry@gmail.com>
2014-05-14 01:09:41 +0200

    New demo with a CAR!!!

---
+++ gdx2d-desktop/src/hevs/gdx2d/demos/physics/car/components/Car.java
@@ -0,0 +1,186 @@
+package hevs.gdx2d.demos.physics.car.components;
+
+import hevs.gdx2d.components.physics.PhysicsBox;
+import hevs.gdx2d.components.physics.utils.PhysicsConstants;
+import hevs.gdx2d.lib.GdxGraphics;
+import hevs.gdx2d.lib.interfaces.DrawableObject;
+import hevs.gdx2d.lib.physics.PhysicsWorld;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Original example adapted from
+ * http://www.level1gamer.com/2012/10/24/top-down-car-using-libgdx-and-box2d/
+ * 
+ * @author Pierre-André Mudry
+ * @version 1.0
+ */
+public class Car implements DrawableObject {
+
+	/**
+	 * Use this to control the car
+	 */
+	public boolean steer_left, steer_right;
+	public boolean accelerate, brake;
+	
+	protected float maxSteerAngle, maxSpeed, power, wheelAngle;
+	protected Body body;
+	protected List<Wheel> wheels;
+
+	/**
+	 * Builds a physically "accurate" car
+	 *  
+	 * @param width The width of the body of the car
+	 * @param length The length of the body of the car
+	 * @param position The start position of the car
+	 * @param angle The starting angle of the car, in radians
+	 * @param power The maximum horse power of the car
+	 * @param maxSteerAngle The maximum steering angle for the wheels, in degrees
+	 * @param maxSpeed The maximum speed for the car
+	 */
+	public Car(float width, float length, Vector2 position, float angle,
+			float power, float maxSteerAngle, float maxSpeed) {
+		
+		this.maxSteerAngle = maxSteerAngle;
+		this.maxSpeed = maxSpeed;
+		this.power = power;
+		
+		World w = PhysicsWorld.getInstance();
+		PhysicsBox carbox = new PhysicsBox("carCenter", position, width/2, length/2, angle);
+		this.body = carbox.getBody();
+		
+		// Initialize wheels
+		this.wheels = new ArrayList<Wheel>();
+		
+		// Topleft wheel
+		this.wheels.add(new Wheel(w, this, -1f, -1.2f, 0.4f, 0.8f, true, true));
+		// Topright wheel
+		this.wheels.add(new Wheel(w, this, 1f, -1.2f, 0.4f, 0.8f, true, true));
+		// Backleft wheel
+		this.wheels.add(new Wheel(w, this, -1f, 1.2f, 0.4f, 0.8f, false, false));
+		// Backright wheel
+		this.wheels.add(new Wheel(w, this, 1f, 1.2f, 0.4f, 0.8f, false, false)); 
+	}
+
+	public List<Wheel> getPoweredWheels() {
+		List<Wheel> poweredWheels = new ArrayList<Wheel>();
+		for (Wheel wheel : this.wheels) {
+			if (wheel.powered)
+				poweredWheels.add(wheel);
+		}
+		return poweredWheels;
+	}
+
+	public Vector2 getLocalVelocity() {
+		/*
+		 * returns car's velocity vector relative to the car
+		 */
+		return this.body.getLocalVector(this.body
+				.getLinearVelocityFromLocalPoint(new Vector2(0, 0)));
+	}
+
+	public List<Wheel> getRevolvingWheels() {
+		List<Wheel> revolvingWheels = new ArrayList<Wheel>();
+		for (Wheel wheel : this.wheels) {
+			if (wheel.revolving)
+				revolvingWheels.add(wheel);
+		}
+		return revolvingWheels;
+	}
+
+	public float getSpeedKMH() {
+		Vector2 velocity = this.body.getLinearVelocity();
+		float len = velocity.len();
+		return (len / 1000) * 3600;
+	}
+
+	public void setSpeed(float speed) {
+		/*
+		 * speed - speed in kilometers per hour
+		 */
+		Vector2 velocity = this.body.getLinearVelocity();
+		velocity = velocity.nor();
+		velocity = new Vector2(velocity.x * ((speed * 1000.0f) / 3600.0f),
+				velocity.y * ((speed * 1000.0f) / 3600.0f));
+		this.body.setLinearVelocity(velocity);
+	}
+
+	public void update(float deltaTime) {
+
+		// 1. KILL SIDEWAYS VELOCITY
+		for (Wheel wheel : wheels) {
+			wheel.killSidewaysVelocity();
+		}
+
+		// 2. SET WHEEL ANGLE
+		// calculate the change in wheel's angle for this update
+		float incr = (this.maxSteerAngle) * deltaTime * 5;
+
+		if (steer_left) {
+			this.wheelAngle = Math.min(Math.max(this.wheelAngle, 0) + incr,
+					this.maxSteerAngle); // increment angle without going over
+											// max steer
+		} else if (steer_right) {
+			this.wheelAngle = Math.max(Math.min(this.wheelAngle, 0) - incr,
+					-this.maxSteerAngle); // decrement angle without going over
+											// max steer
+		} else {
+			this.wheelAngle = 0;
+		}
+
+		// update revolving wheels
+		for (Wheel wheel : this.getRevolvingWheels()) {
+			wheel.setAngle(this.wheelAngle);
+		}
+
+		// 3. APPLY FORCE TO WHEELS
+		Vector2 baseVector; // vector pointing in the direction force will be
+							// applied to a wheel ; relative to the wheel.
+
+		// if accelerator is pressed down and speed limit has not been reached,
+		// go forwards
+		if ((accelerate) && (this.getSpeedKMH() < this.maxSpeed)) {
+			baseVector = new Vector2(0, -1);
+		} else if (brake) {
+			// braking, but still moving forwards - increased force
+			if (this.getLocalVelocity().y < 0)
+				baseVector = new Vector2(0f, 1.3f);
+			// going in reverse - less force
+			else
+				baseVector = new Vector2(0f, 0.7f);
+		} else {
+			// slow down if not accelerating
+			baseVector = new Vector2(0, 0);
+			if (this.getSpeedKMH() < 7)
+				this.setSpeed(0);
+			else if (this.getLocalVelocity().y < 0)
+				baseVector = new Vector2(0, 0.7f);
+			else if (this.getLocalVelocity().y > 0)
+				baseVector = new Vector2(0, -0.7f);
+		}
+		// multiply by engine power, which gives us a force vector relative to
+		// the wheel
+		Vector2 forceVector = new Vector2(this.power * baseVector.x, this.power
+				* baseVector.y);
+
+		// apply force to each wheel
+		for (Wheel wheel : this.getPoweredWheels()) {
+			Vector2 position = wheel.body.getWorldCenter();
+			wheel.body.applyForce(wheel.body.getWorldVector(new Vector2(
+					forceVector.x, forceVector.y)), position, true);
+		}
+	}
+
+	@Override
+	public void draw(GdxGraphics g) {
+		Vector2 pos = PhysicsConstants.coordMetersToPixels(body.getPosition());
+		g.drawFilledCircle(pos.x, pos.y, 10, Color.BLUE);
+	}
+
+}
---
+++ gdx2d-desktop/src/hevs/gdx2d/demos/physics/car/components/Wheel.java
@@ -0,0 +1,104 @@
+package hevs.gdx2d.demos.physics.car.components;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.joints.PrismaticJointDef;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
+
+public class Wheel {	
+	public Car car;//car this wheel belongs to	
+	private float width; // width in meters
+	private float length; // length in meters
+	public boolean revolving; // does this wheel revolve when steering?
+	public boolean powered; // is this wheel powered?
+	public Body body;
+
+	public Wheel(World world, Car car, float posX, float posY, float width, float length,
+			boolean revolving, boolean powered) {
+		super();
+		this.car = car;
+		this.width = width;
+		this.length = length;
+		this.revolving = revolving;
+		this.powered = powered;
+		
+		//init body 
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		bodyDef.position.set(car.body.getWorldPoint(new Vector2(posX, posY)));
+		bodyDef.angle = car.body.getAngle();
+		this.body = world.createBody(bodyDef);
+
+		//init shape
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.density = 1.0f;
+		fixtureDef.isSensor=true; //wheel does not participate in collision calculations: resulting complications are unnecessary
+		PolygonShape wheelShape = new PolygonShape();
+		wheelShape.setAsBox(this.width/2, this.length/2);
+		fixtureDef.shape = wheelShape;
+		this.body.createFixture(fixtureDef);
+		wheelShape.dispose();
+		
+	    //create joint to connect wheel to body
+	    if(this.revolving){
+	    	RevoluteJointDef jointdef=new RevoluteJointDef();
+	        jointdef.initialize(this.car.body, this.body, this.body.getWorldCenter());
+	        jointdef.enableMotor=false; //we'll be controlling the wheel's angle manually
+		    world.createJoint(jointdef);
+	    }else{
+	    	PrismaticJointDef jointdef=new PrismaticJointDef();
+	        jointdef.initialize(this.car.body, this.body, this.body.getWorldCenter(), new Vector2(1, 0));
+	        jointdef.enableLimit=true;
+	        jointdef.lowerTranslation=jointdef.upperTranslation=0;
+		    world.createJoint(jointdef);
+	    }
+	}
+	
+	public void setAngle (float angle){
+	    /*
+	    angle - wheel angle relative to car, in degrees
+	    */
+		this.body.setTransform(body.getPosition(), this.car.body.getAngle() + (float) Math.toRadians(angle));
+	};
+
+	public Vector2 getLocalVelocity () {
+	    /*returns get velocity vector relative to car
+	    */
+	    return this.car.body.getLocalVector(this.car.body.getLinearVelocityFromLocalPoint(this.body.getPosition()));
+	};
+
+	public Vector2 getDirectionVector () {
+	    /*
+	    returns a world unit vector pointing in the direction this wheel is moving
+	    */
+		Vector2 directionVector;
+		if (this.getLocalVelocity().y > 0)
+			directionVector = new Vector2(0,1);
+		else
+			directionVector = new Vector2(0,-1);
+			
+		return directionVector.rotate((float) Math.toDegrees(this.body.getAngle()));	    
+	};
+
+
+	public Vector2 getKillVelocityVector (){
+	    /*
+	    substracts sideways velocity from this wheel's velocity vector and returns the remaining front-facing velocity vector
+	    */
+	    Vector2 velocity = this.body.getLinearVelocity();
+	    Vector2 sidewaysAxis=this.getDirectionVector();
+	    float dotprod = velocity.dot(sidewaysAxis);
+	    return new Vector2(sidewaysAxis.x*dotprod, sidewaysAxis.y*dotprod);
+	};
+
+	public void killSidewaysVelocity (){
+	    /*
+	    removes all sideways velocity from this wheels velocity
+	    */
+	    this.body.setLinearVelocity(this.getKillVelocityVector());
+	};
+}
