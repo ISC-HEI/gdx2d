8bf0c0faf55e99e3f66335003a0865237f5f1544
Pierre-André Mudry <pmudry@gmail.com>
2014-05-14 01:00:03 +0200

    Minor type

--- gdx2d-desktop/src/hevs/gdx2d/demos/physics/joints/DemoRopeJoint.java
+++ gdx2d-desktop/src/hevs/gdx2d/demos/physics/joints/DemoRopeJoint.java
@@ -11,7 +11,6 @@ import hevs.gdx2d.lib.physics.PhysicsWorld;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
 
