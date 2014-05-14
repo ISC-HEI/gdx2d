58c339cf13d4ddeec21d4b5f8fcf0408490d2b72
Pierre-André Mudry <pmudry@gmail.com>
2014-05-14 15:54:23 +0200

    New function to read image color, fix #67

--- gdx2d-desktop/src/hevs/gdx2d/components/bitmaps/BitmapImage.java
+++ gdx2d-desktop/src/hevs/gdx2d/components/bitmaps/BitmapImage.java
@@ -2,10 +2,16 @@ package hevs.gdx2d.components.bitmaps;
 
 import hevs.gdx2d.lib.utils.Utils;
 
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.PixmapTextureData;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 
 /**
@@ -23,6 +29,9 @@ final public class BitmapImage implements Disposable{
 	protected Texture image;
 	protected TextureRegion tRegion;
 	
+	protected String filePath;
+	protected BufferedImage cleanImage;
+
 	/**
 	 * Use the {@link BitmapImageFactory} to create instances of this class
 	 * @see BitmapImageFactory
@@ -35,6 +44,8 @@ final public class BitmapImage implements Disposable{
 		image = new Texture(Gdx.files.internal(file));
 		tRegion = new TextureRegion(image);
 		image.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+		filePath = file;
 	}
 
 	/**
@@ -65,6 +76,73 @@ final public class BitmapImage implements Disposable{
 	 */
 	public void mirrorUpDown(){
 		tRegion.flip(false, true);
+	}
+
+	
+	/**
+	 * @param p The pixel position, in image space
+	 * @return If the pixel can be contained in the image space
+	 * @see BitmapImage#isContained(int, int)
+	 */
+	public boolean isContained(Vector2 p){
+		return isContained((int)p.x, (int)p.y);
+	}
+	
+	/**
+	 * Is a given pixel position contained in the image or not
+	 * The pixel is given in image space (width x height)
+	 * @param x x-position of the pixel
+	 * @param y y-position of the pixel
+	 * @return if the pixel is contained inside the image
+	 */
+	public boolean isContained(int x, int y){
+		if(x < 0 || y < 0)
+			return false;
+		
+		if(x >= image.getWidth())
+			return false;
+		
+		if(y >= image.getHeight())
+			return false;
+		
+		return true;
+	}
+	
+	/**
+	 * To get the color of a pixel in an image, we
+	 * must get translate screen coordinates to image coordinates
+	 * This is the role of the following function. 
+	 * @param pixelPosition The pixel we want to read, in screen coordinates
+	 * @param imagePosition The current position of the image, in screen coordinates
+	 * @return The pixel position in the image space coordinates 
+	 */	
+	public Vector2 pixelInScreenSpace(Vector2 pixelPosition, Vector2 imagePosition){
+		int width = image.getWidth();
+		int height = image.getWidth();
+		
+		Vector2 center = pixelPosition.sub(imagePosition);
+		center.add(width/2, height/2);
+		
+		return center;
+	}
+	
+	/**
+	 * Returns the current color of an image
+	 * @param x The x position we want to read
+	 * @param y The y position we want to read
+	 * @return The color read
+	 */
+	public int getColor(int x, int y){
+		// Read the image only if needed
+		if(cleanImage == null){
+			try{
+				cleanImage = ImageIO.read(Gdx.files.internal(filePath).file()); 
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}		
+		return cleanImage.getRGB(x, cleanImage.getHeight() - y);
 	}
 	
 	/**
