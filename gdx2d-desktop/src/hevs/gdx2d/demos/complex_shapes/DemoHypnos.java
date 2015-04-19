ebead8f52f855a91b93328899cf247da602969f7
Pierre-André Mudry <pmudry@gmail.com>
2015-04-19 23:36:33 +0200

    First try at doing the Hypnos demo #36

---
+++ gdx2d-desktop/src/hevs/gdx2d/demos/complex_shapes/DemoHypnos.java
@@ -0,0 +1,106 @@
+package hevs.gdx2d.demos.complex_shapes;
+
+import com.badlogic.gdx.graphics.Color;
+import hevs.gdx2d.components.geometry.Point;
+import hevs.gdx2d.lib.GdxGraphics;
+import hevs.gdx2d.lib.PortableApplication;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * First try at reproducing http://lab.hakim.se/hypnos/
+ * Not done yet...
+ *
+ * @author Pierre-André Mudry, mui
+ * @version 1.0, April 2015
+ */
+
+class Coord {
+    Coord(float x, float y, float r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    float x, y, r;
+}
+
+public class DemoHypnos extends PortableApplication {
+
+    private float quality = 100;
+    private float layerSize;
+    private float layerOverlap = Math.round((quality * 0.2));
+
+    private float radius;
+    private int screenWidth, screenHeight;
+    private float angle = 0;
+
+    final ArrayList<Coord> points = new ArrayList<Coord>();
+
+    protected void generateObjects() {
+        for (int i = 0; i < quality; i++) {
+            float x = screenWidth / 2  + (float)Math.sin(i / quality * 2 * Math.PI) * radius - layerSize;
+            float y = screenHeight / 2 + (float)Math.cos(i / quality * 2 * Math.PI) * radius - layerSize;
+            float r = (float)((i / quality) * Math.PI);
+            Coord c = new Coord(x, y, r);
+            points.add(c);
+        }
+    }
+
+    @Override
+    public void onInit() {
+        this.setTitle("Demo shapes, mui 2013");
+        screenWidth = getWindowWidth();
+        screenHeight = getWindowHeight();
+        radius = Math.min(screenWidth, screenHeight) * 0.2f;
+        layerSize = radius * 0.25f;
+        generateObjects();
+    }
+
+    private void update(){
+        for(Coord c : points){
+            c.r += 0.3f;
+        }
+    }
+
+    @Override
+    public void onGraphicRender(GdxGraphics g) {
+        angle = angle >= 360 ? 0 : angle + 0.2f;
+        g.clear(Color.WHITE);
+        update();
+
+        for(Coord c : points){
+            g.setColor(Color.WHITE);
+            g.drawFilledRectangle(c.x, c.y, 50, 50, c.r);
+            g.setColor(Color.BLACK);
+            g.drawRectangle(c.x, c.y, 50, 50, c.r);
+        }
+
+        Coord beg = points.get(0);
+        g.setColor(Color.WHITE);
+        g.drawFilledRectangle(beg.x, beg.y, 50, 50, beg.r);
+        g.setColor(Color.BLACK);
+        g.drawRectangle(beg.x, beg.y, 50, 50, beg.r);
+
+
+        for(int i = (int)(quality - 30); i < quality; i++){
+            Coord c = points.get(i);
+            g.setColor(Color.WHITE);
+            g.drawFilledRectangle(c.x, c.y, 50, 50, c.r);
+            g.setColor(Color.BLACK);
+            g.drawRectangle(c.x, c.y, 50, 50, c.r);
+        }
+
+        g.drawSchoolLogo();
+        g.drawFPS();
+    }
+
+    public DemoHypnos(boolean onAndroid) {
+        super(onAndroid);
+    }
+
+    public static void main(String[] args) {
+        new DemoHypnos(false);
+    }
+}
