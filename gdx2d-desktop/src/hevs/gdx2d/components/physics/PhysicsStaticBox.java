61e5a7850d9bcf22d4f5a71c23522091c6d1dbc0
Pierre-André Mudry <pmudry@gmail.com>
2015-04-21 16:45:11 +0200

    Fixed bug from P. Roduit where constructor where all messed up.

--- gdx2d-desktop/src/hevs/gdx2d/components/physics/PhysicsStaticBox.java
+++ gdx2d-desktop/src/hevs/gdx2d/components/physics/PhysicsStaticBox.java
@@ -11,9 +11,13 @@ import com.badlogic.gdx.physics.box2d.Shape.Type;
  * <b>Note:</b> all dimensions and positions are in pixels.
  *
  * @author Pierre-André Mudry (mui)
- * @version 1.0
+ * @version 1.0.1
  */
 public class PhysicsStaticBox extends AbstractPhysicsObject {
+
+    private static final float DEFAULT_DENSITY = 20;
+    private static final float DEFAULT_RESTITUTION = 0.1f;
+    private static final float DEFAULT_FRICTION = 0.3f;
 
     /**
      * Create a static box.
@@ -24,7 +28,7 @@ public class PhysicsStaticBox extends AbstractPhysicsObject {
      * @param height   The height of the physics box
      */
     public PhysicsStaticBox(String name, Vector2 position, float width, float height) {
-        super(Type.Polygon, name, position, width, height, 20, 0.1f, 0.3f, false);
+        super(Type.Polygon, name, position, width, height, DEFAULT_DENSITY ,DEFAULT_RESTITUTION, DEFAULT_FRICTION, false);
     }
 
     /**
@@ -37,6 +41,6 @@ public class PhysicsStaticBox extends AbstractPhysicsObject {
      * @param angle    The angle of the box (trig angle)
      */
     public PhysicsStaticBox(String name, Vector2 position, float width, float height, float angle) {
-        super(Type.Polygon, name, position, width, height, angle, 0.1f, 0.3f, angle, false);
+        super(Type.Polygon, name, position, width, height, DEFAULT_DENSITY , DEFAULT_RESTITUTION, DEFAULT_FRICTION, angle, false);
     }
 }
