4f0f18c24a4477399e7ee424a68bbe329363ffeb
Pierre-André Mudry <pmudry@gmail.com>
2015-04-22 09:16:21 +0200

    Updated for 1.0.1-SNAPSHOT release
    In preparation for the new build.

--- gdx2d-helloDesktop/src/hevs/gdx2d/hello/HelloWorld.java
+++ gdx2d-helloDesktop/src/hevs/gdx2d/hello/HelloWorld.java
@@ -1,15 +1,14 @@
 package hevs.gdx2d.hello;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Interpolation;
 import hevs.gdx2d.components.bitmaps.BitmapImage;
 import hevs.gdx2d.lib.GdxGraphics;
 import hevs.gdx2d.lib.PortableApplication;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Interpolation;
-
 /**
  * Hello World demo.
- * 
+ *
  * @author Christopher Métrailler (mei)
  * @author Pierre-André Mudry (mui)
  * @version 1.0
@@ -41,36 +40,36 @@ public class HelloWorld extends PortableApplication {
 	public void onGraphicRender(GdxGraphics g) {
 		// Clears the screen
 		g.clear();
-		
+
 		// Compute the angle of the image using an elastic interpolation
-		float t = computePercentage();		
+		float t = computePercentage();
 		float angle = Interpolation.elastic.apply(MIN_ANGLE, MAX_ANGLE, t);
-		
+
 		// Draw everything
-		g.drawTransformedPicture(getWindowWidth() / 2.0f, getWindowHeight() / 2.0f, angle, 1.0f,imgBitmap);
+		g.drawTransformedPicture(getWindowWidth() / 2.0f, getWindowHeight() / 2.0f, angle, 1.0f, imgBitmap);
 		g.drawStringCentered(getWindowHeight() * 0.8f, "Welcome to GDX2D!");
 		g.drawFPS();
 		g.drawSchoolLogo();
 	}
-	
+
 	/**
 	 * Compute time percentage for making
 	 * a looping animation
+	 *
 	 * @return the current normalized time
 	 */
-	private float computePercentage()
-	{
-		if(direction == 1)
+	private float computePercentage() {
+		if (direction == 1)
 			currentTime += Gdx.graphics.getDeltaTime();
 		else
 			currentTime -= Gdx.graphics.getDeltaTime();
-		
-		if(currentTime >= ANIMATION_LENGTH || currentTime <= 0)
+
+		if (currentTime >= ANIMATION_LENGTH || currentTime <= 0)
 			direction *= -1;
-				
+
 		return currentTime / ANIMATION_LENGTH;
 	}
-	
+
 	public static void main(String[] args) {
 		new HelloWorld(false);
 	}
