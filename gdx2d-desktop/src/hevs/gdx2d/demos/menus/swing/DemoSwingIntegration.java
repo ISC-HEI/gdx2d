df4e734fbbb1a9529786b79ac5f900ea948413fa
Pierre-André Mudry <pmudry@gmail.com>
2015-05-04 14:31:42 +0200

    Taming LwjglAWT, v1

--- gdx2d-desktop/src/hevs/gdx2d/demos/menus/swing/DemoSwingIntegration.java
+++ gdx2d-desktop/src/hevs/gdx2d/demos/menus/swing/DemoSwingIntegration.java
@@ -2,23 +2,23 @@ package hevs.gdx2d.demos.menus.swing;
 
 
 import com.badlogic.gdx.backends.lwjgl.LwjglAWTCanvas;
-
 import hevs.gdx2d.demos.physics.DemoSimplePhysics;
 import hevs.gdx2d.demos.simple.DemoCircles;
 import hevs.gdx2d.lib.Game2D;
 import hevs.gdx2d.lib.PortableApplication;
 
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class DemoSwingIntegration extends JFrame {
-	LwjglAWTCanvas canvas2;
 
 	int current = 0;
+
+	ArrayList<LwjglAWTCanvas> canvasList = new ArrayList<LwjglAWTCanvas>();
 
 	public DemoSwingIntegration() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -30,37 +30,39 @@ public class DemoSwingIntegration extends JFrame {
 
 		PortableApplication.CreateLwjglApplication = false; // TODO: configuration this way is ugly...
 
-		canvas2 = new LwjglAWTCanvas(new Game2D(new DemoSimplePhysics()));
-		canvas2.getCanvas().setSize(400, 400);
-		container.add(canvas2.getCanvas());
-
 		JPanel jp1 = new JPanel();
 		jp1.setLayout(new FlowLayout());
 		JButton jb1 = new JButton("Change demo");
+		jp1.add(jb1);
+		container.add(jp1);
+
+		LwjglAWTCanvas canvas = new LwjglAWTCanvas(new Game2D(new DemoSimplePhysics()));
+		canvas.getCanvas().setSize(400, 400);
+		container.add(canvas.getCanvas());
+		canvasList.add(canvas);
 
 		jb1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent actionEvent) {
-				if (canvas2 == null) {
-					if ((current % 2) == 1)
-						canvas2 = new LwjglAWTCanvas(new Game2D(new DemoSimplePhysics()));
-					else
-						canvas2 = new LwjglAWTCanvas(new Game2D(new DemoCircles()));
+				canvasList.get(0).stop();
 
-					canvas2.getCanvas().setSize(400, 400);
-					container.add(canvas2.getCanvas());
-					pack();
-				} else {
-					canvas2.stop();
-					container.remove(canvas2.getCanvas());
-					canvas2 = null;
-					current++;
-				}
+				LwjglAWTCanvas canvas;
+				if ((current % 2) == 1)
+					canvas = new LwjglAWTCanvas(new Game2D(new DemoSimplePhysics()), null);
+				else
+					canvas = new LwjglAWTCanvas(new Game2D(new DemoCircles()), null);
+
+				current++;
+
+				canvasList.clear();
+				canvasList.add(0, canvas);
+
+				container.remove(1);
+				canvas.getCanvas().setSize(400, 400);
+				container.add(canvas.getCanvas());
+				pack();
 			}
 		});
-
-		jp1.add(jb1);
-		container.add(jp1);
 
 		setSize(600, 500);
 		setLocationRelativeTo(null);
