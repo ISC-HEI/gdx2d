c4cc46372dbda51543d27b29d4bd61ceeb4db086
Christopher Métrailler <christopher.metrailler@hevs.ch>
2016-04-01 14:17:02 +0200

    Bump gdx2d library version 1.2.0. Release gdx2d-core and gdx2d-desktop as version 1.2.0 on Maven Central.

--- gdx2d-library/gdx2d-core/src/main/java/ch/hevs/gdx2d/lib/Version.java
+++ gdx2d-library/gdx2d-core/src/main/java/ch/hevs/gdx2d/lib/Version.java
@@ -1,40 +1,38 @@
 package ch.hevs.gdx2d.lib;
 
 /**
- * Library version information.
- *
- * @author Pierre-André Mudry
+ * Core library version.
  */
 public class Version {
 
-	private final static String COPY = "| mui, chn, mei, pim (c) 2012-2015";
+  private final static String COPY = "| mui, chn, mei, pim (c) 2012-2016";
 
-	/**
-	 * Current version name of the gdx2d library (major.minor.revision).
-	 */
-	public static final String VERSION = "1.1.2-SNAPSHOT";
+  /**
+   * Current version name of the gdx2d library (major.minor.revision).
+   */
+  public final static String VERSION = "1.2.0";
 
-	/**
-	 * Indicates if if it is a debug or release version.
-	 */
-	public final static boolean isSnapshot = true;
+  /**
+   * Indicates if if it is a debug or release version.
+   */
+  public final static boolean isSnapshot = true;
 
 
-	/**
-	 * Print the current version of {@code libgdx} and {@code gdx2d}.
-	 *
-	 * @return the version of {@code libgdx} and {@code gdx2d}
-	 */
-	public static String printVerbose() {
-		return String.format("gdx2d-lib v%s, libgdx v%s %s", VERSION, com.badlogic.gdx.Version.VERSION, COPY);
-	}
+  /**
+   * Print the current version of {@code libgdx} and {@code gdx2d}.
+   *
+   * @return the version of {@code libgdx} and {@code gdx2d}
+   */
+  public static String printVerbose() {
+    return String.format("gdx2d-core v%s, libgdx v%s %s", VERSION, com.badlogic.gdx.Version.VERSION, COPY);
+  }
 
-	/**
-	 * Print the current version of {@code gdx2d}.
-	 *
-	 * @return the version of {@code gdx2d}
-	 */
-	public static String print() {
-		return String.format("gdx2d-lib v%s %s", VERSION, COPY);
-	}
+  /**
+   * Print the current version of {@code gdx2d}.
+   *
+   * @return the version of {@code gdx2d}
+   */
+  public static String print() {
+    return String.format("gdx2d-core v%s %s", VERSION, COPY);
+  }
 }
