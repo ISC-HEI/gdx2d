fa28b2627d7aab991d574c9fb6f7b135df03735d
Christopher Métrailler <christopher.metrailler@hevs.ch>
2016-06-09 08:37:55 +0200

    Controllers demo improved. #131 Draw an XBox One controller and the state of some buttons. Sticks drawing also improved.

--- gdx2d-demoDesktop/src/ch/hevs/gdx2d/demos/controllers/DemoControllers.java
+++ gdx2d-demoDesktop/src/ch/hevs/gdx2d/demos/controllers/DemoControllers.java
@@ -1,5 +1,6 @@
 package ch.hevs.gdx2d.demos.controllers;
 
+import ch.hevs.gdx2d.components.bitmaps.BitmapImage;
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.controllers.PovDirection;
@@ -8,9 +9,23 @@ import ch.hevs.gdx2d.desktop.PortableApplication;
 import ch.hevs.gdx2d.desktop.Xbox;
 import ch.hevs.gdx2d.lib.GdxGraphics;
 import ch.hevs.gdx2d.lib.utils.Logger;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Demo program for the {@code controllers} extension.
+ *
+ * If you use the XBox One controller, see
+ * <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/360_controller.svg/450px-360_controller.svg.png">
+ * this image</a> which describes each button and axes.
+ *
+ * The XBox One controller image comes <a href="from https://thenounproject.com/term/xbox-one/45114/">here</a> (CC BY
+ * 3.0 US).
  *
  * @author Pierre-André Mudry (mui)
  * @author Christopher Metrailler (mei)
@@ -20,96 +35,134 @@ public class DemoControllers extends PortableApplication {
 
 	private static final String TAG = DemoControllers.class.getSimpleName();
 
-	public static void main(String[] args) {
-		new DemoControllers();
+	private BitmapImage background;
+	private final Vector2 leftSickVal = Vector2.Zero.cpy(); // X,Y values of the left stick (POV)
+	private final Vector2 rightSickVal = Vector2.Zero.cpy(); // X,Y values of the left stick (POV)
+
+	private Vector2 leftStickPos; // Center coordinates of the left stick
+	private Vector2 rightStickPos; // Center coordinates of the right stick
+
+	private Vector2 endPos = new Vector2();
+	private Vector2 center;
+
+  // Contain some button positions used to draw a circle when they are pressed
+	private static Map<Integer, Vector2> buttonPos = new HashMap<Integer, Vector2>();
+	static {
+		buttonPos.put(Xbox.X, new Vector2(85, 97));
+		buttonPos.put(Xbox.Y, new Vector2(113, 123));
+    buttonPos.put(Xbox.A, new Vector2(113, 71));
+		buttonPos.put(Xbox.B, new Vector2(141, 97));
+	}
+
+	private Controller ctrl; // The connected controller
+
+	private DemoControllers() {
+		super(700, 700, false);
 	}
 
 	@Override
 	public void onInit() {
-		setTitle("Controllers demo, mui / mei 2016");
+		setTitle("Controllers demo, mui, mei 2016");
 
 		Logger.log(TAG, "%d controller(s) found", Controllers.getControllers().size);
 		for (Controller controller : Controllers.getControllers()) {
 			Logger.log(TAG, " - " + controller.getName());
 		}
+		ctrl = Controllers.getControllers().first();
 
-		leftDisplayPosX = this.getWindowWidth() / 2 - 100;
-		leftDisplayPosY = this.getWindowHeight() / 2;
+		background = new BitmapImage("data/images/noun_45114_cc.png");
 
-		rightDisplayPosX = this.getWindowWidth() / 2 + 100;
-		rightDisplayPosY = this.getWindowHeight() / 2;
-
+		center = new Vector2(getWindowWidth(), getWindowHeight()).scl(0.5f);
+		leftStickPos = new Vector2(-105, 97).add(center);
+		rightStickPos = new Vector2(58, 33).add(center);
 	}
-
-	float xLeft, yLeft;
-	float xRight, yRight;
-
-	int leftDisplayPosX, leftDisplayPosY;
-	int rightDisplayPosX, rightDisplayPosY;
 
 	@Override
 	public void onGraphicRender(GdxGraphics g) {
-		g.clear();
+		g.clear(Color.WHITE);
 
-		// Left controller display
-		float end_x = leftDisplayPosX + xLeft * 80;
-		float end_y = leftDisplayPosY - yLeft * 80;
-		g.drawLine(leftDisplayPosX, leftDisplayPosY, end_x, end_y);
+		if (ctrl == null)
+			return;
 
-		// Right controller display
-		end_x = rightDisplayPosX + xRight * 80;
-		end_y = rightDisplayPosY - yRight * 80;
-		g.drawLine(rightDisplayPosX, rightDisplayPosY, end_x, end_y);
+		g.drawBackground(background, 0, 0);
 
-		g.drawFPS();
+		g.setColor(Color.RED);
+		g.setPenWidth(3);
+
+		// Left stick display
+		endPos.x = leftSickVal.x * 34;
+		endPos.y = leftSickVal.y * 34;
+		endPos = endPos.limit(34); // Make a nice round
+		g.drawLine(leftStickPos.x, leftStickPos.y, leftStickPos.x + endPos.x, leftStickPos.y - endPos.y);
+
+		// Right stick display
+		endPos.x = rightSickVal.x * 34;
+		endPos.y = rightSickVal.y * 34;
+		endPos = endPos.limit(34); // Make a nice round
+		g.drawLine(rightStickPos.x, rightStickPos.y, rightStickPos.x + endPos.x, rightStickPos.y - endPos.y);
+
+		// Buttons clicked
+    for (Integer buttonCode : buttonPos.keySet()) {
+      if (ctrl.getButton(buttonCode)) {
+        Vector2 pos = buttonPos.get(buttonCode).cpy().add(center);
+        g.drawFilledCircle(pos.x, pos.y, 10, Color.RED);
+      }
+    }
+
+		g.drawFPS(Color.BLACK);
 		g.drawSchoolLogo();
 	}
 
 	@Override
 	public void onControllerConnected(Controller controller) {
-		Logger.log("A controller has been connected !");
+		Logger.log(TAG, "A controller has been connected !");
+    ctrl = controller;
 	}
 
-	@Override
+  @Override
+  public void onControllerDisconnected(Controller controller) {
+    ctrl = null;
+  }
+
+  @Override
 	public void onControllerKeyDown(Controller controller, int buttonCode) {
 		if (buttonCode == Xbox.X)
-			Logger.log("Button X pressed");
+			Logger.log(TAG, "Button X pressed");
 		if (buttonCode == Xbox.Y)
-			Logger.log("Button Y pressed");
+			Logger.log(TAG, "Button Y pressed");
 		if (buttonCode == Xbox.A)
-			Logger.log("Button A pressed");
+			Logger.log(TAG, "Button A pressed");
 		if (buttonCode == Xbox.B)
-			Logger.log("Button B pressed");
+			Logger.log(TAG, "Button B pressed");
 		if (buttonCode == Xbox.L_TRIGGER)
-			Logger.log("Button L trigger pressed");
+			Logger.log(TAG, "Button L trigger pressed");
 		if (buttonCode == Xbox.R_TRIGGER)
-			Logger.log("Button R trigger pressed");
-
+			Logger.log(TAG, "Button R trigger pressed");
 	}
 
 	@Override
 	public void onControllerAxisMoved(Controller controller, int axisCode, float value) {
 		super.onControllerAxisMoved(controller, axisCode, value);
 
-		if (axisCode == Xbox.L_STICK_HORIZONTAL_AXIS) {
-			xLeft = value;
-		}
+		if (axisCode == Xbox.L_STICK_HORIZONTAL_AXIS)
+			leftSickVal.x = value;
 
-		if (axisCode == Xbox.L_STICK_VERTICAL_AXIS) {
-			yLeft = value;
-		}
+		if (axisCode == Xbox.L_STICK_VERTICAL_AXIS)
+			leftSickVal.y = value;
 
-		if (axisCode == Xbox.R_STICK_HORIZONTAL_AXIS) {
-			xRight = value;
-		}
+		if (axisCode == Xbox.R_STICK_HORIZONTAL_AXIS)
+			rightSickVal.x = value;
 
-		if (axisCode == Xbox.R_STICK_VERTICAL_AXIS) {
-			yRight = value;
-		}
+		if (axisCode == Xbox.R_STICK_VERTICAL_AXIS)
+			rightSickVal.y = value;
 	}
 
 	@Override
 	public void onControllerPovMoved(Controller controller, int povCode, PovDirection value) {
-		Logger.log("POV: " + value);
+		Logger.log(TAG, "POV: " + value);
+	}
+
+	public static void main(String[] args) {
+		new DemoControllers();
 	}
 }
